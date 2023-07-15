f1b9e8904dfd98b6c91fa5a752905ce144d05812
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2023-07-15 10:55:37 -0700

    feat(tailwind):  extend colors

--- pruebas/01-reading-list/sam-621/tailwind.config.js
+++ pruebas/01-reading-list/sam-621/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/ui/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        viewport: '#161C24',
+        primary: '#FA531B',
+        neutral: '#F9F9F9'
+      }
+    }
   },
   plugins: []
 }
